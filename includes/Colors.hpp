c693c8423770e701eafee8ed0aa881287d7521bf
ldalmass <ldalmass@student.42nice.fr>
2025-03-13 15:36:20 +0100

    ldalmass: includes/Colors.hpp: Aligned colors cause im maniac

--- includes/Colors.hpp
+++ includes/Colors.hpp
@@ -6,21 +6,21 @@
 /*   By: ldalmass <ldalmass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 12:18:44 by hugolefevre       #+#    #+#             */
-/*   Updated: 2025/03/13 14:35:47 by ldalmass         ###   ########.fr       */
+/*   Updated: 2025/03/13 15:35:12 by ldalmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_HPP
 #define COLORS_HPP
 
-# define RESET "\033[0m"
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define BLUE "\033[0;34m"
-# define YELLOW "\033[0;33m"
+# define RESET	"\033[0m"
+# define RED	"\033[0;31m"
+# define GREEN	"\033[0;32m"
+# define BLUE	"\033[0;34m"
+# define YELLOW	"\033[0;33m"
 
-// # define INFO "\033[0;34m[INFO]\033[0m  "
-// # define DEBUG "\033[0;33m[DEBUG]\033[0m "
-// # define ERROR "\033[0;31m[ERROR]\033[0m "
+// # define INFO	"\033[0;34m[INFO]\033[0m  "
+// # define DEBUG	"\033[0;33m[DEBUG]\033[0m "
+// # define ERROR	"\033[0;31m[ERROR]\033[0m "
 
 #endif
