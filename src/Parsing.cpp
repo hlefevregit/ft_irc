8c2b54af58e5b17755f3b1a05cbc94b959186a1a
ldalmass <ldalmass@student.42nice.fr>
2025-03-28 17:52:08 +0100

    ldalmass: src/Parsing.cpp: Uncommented line 141. Fixes buffer not having next command to process but last one

--- src/Parsing.cpp
+++ src/Parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: ldalmass <ldalmass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 10:22:27 by hugolefevre       #+#    #+#             */
-/*   Updated: 2025/03/28 17:31:53 by ldalmass         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:48:08 by ldalmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,9 @@ static void splitMessage(std::vector<std::string> &cmds, std::string msg)
 int	Server::parseMessage(Client *client, std::string const &message, std::vector<pollfd> &pollfds)
 {
 	AUTO_LOG
-	// std::cout << INFO IN << "parseMessage" << TRAIL << std::endl;
 	std::vector<std::string>				cmds;
 	std::map<const int, Client>::iterator	sender = _clients.find(client->getFd());
 	
-	// std::cerr << DEBUG << "│  nb commands : "  << std::endl;
 	splitMessage(cmds, message);
 	
 	for (size_t i = 0; i != cmds.size(); i++)
@@ -140,10 +138,9 @@ int	Server::parseMessage(Client *client, std::string const &message, std::vector
 				// 		botParse(sender->second, cmd.params);
 				// 		sender->second.sendMessage(sender->second, cmd.params);
 			}
-			// client->resetBuffer();
+			client->resetBuffer();
 		}
 	}
 
-	// std::cout << INFO OUT << "parseMessage" << TRAIL << std::endl;
 	return 0;
 }
